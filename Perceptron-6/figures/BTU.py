624e6498a062f381a5e1805cfcb54a9b85afef5e
Injamuri Krutika <kruteeka.injamuri@gmail.com>
2018-12-04 10:19:49 +0530

    Add Activation notes

---
+++ Perceptron-6/figures/BTU.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+def display(xaxis,yaxis):
+    plt.plot(xaxis,yaxis)
+    plt.grid()
+    plt.show()
+
+def BTU(input):
+    if input>=0:
+        return 1
+    else:
+        return 0
+x = list(np.arange(-10.0,10.0,0.0001))
+y = []
+for val in x:
+    y.append(BTU(val))
+display(x,y)
