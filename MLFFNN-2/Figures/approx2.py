af5e1daeaaf491bdb91d7617c823fab103819a7b
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-12-08 13:49:22 +0530

    Update code for better sigmoids and shading

--- MLFFNN-2/Figures/approx2.py
+++ MLFFNN-2/Figures/approx2.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def f(x):
-    return 1 if x >= 4 and x <= 6 else 0
+    return 1 if x >= 2 and x <= 6 else 0
 
 
 def sigmoid(x):
@@ -12,16 +12,23 @@ def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
 
-# Plot both the f(x) and sigmoid
-plt.xlim(0, 10)
-plt.ylim(0, 2)
-x1 = np.linspace(0, 10, 8000)
-f = np.vectorize(f)
-y1 = f(x1)
-x2 = np.linspace(5, 7, 2000)  # Large weights
-sigmoid = np.vectorize(sigmoid)
-y2 = f(x2)
-plt.plot(x1, y1, color='b', lw=2)
-plt.plot(x2, y2, color='g', lw=2)
-plt.title("Approximating f(x)")
-plt.show()
+def draw():
+    global sigmoid, f
+    x1 = np.linspace(0, 10, 8000)
+    f = np.vectorize(f)
+    y1 = f(x1)
+    x = np.arange(-20, 20, 0.01)
+    w = 3
+    bais = 18.5
+    z = x * w
+    z -= bais
+    sigmoid = np.vectorize(sigmoid)
+    y2 = sigmoid(-z)
+    plt.plot(x, y2, color='r', lw=2, label="Sigmoid")
+    plt.plot(x1, y1, color='b', lw=2, label="f(x)")
+    plt.fill_between(x, [0], y2)
+    plt.xlim(0, 10)
+    plt.title("Approximation 2")
+    plt.legend()
+    plt.grid()
+    plt.show()
--- MLFFNN-2/Figures/approx3.py
+++ MLFFNN-2/Figures/approx3.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def f(x):
-    return 1 if x >= 4 and x <= 6 else 0
+    return 1 if x >= 2 and x <= 6 else 0
 
 
 def sigmoid(x):
@@ -12,18 +12,33 @@ def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
 
-# Plot both the f(x) and sigmoid
-plt.xlim(0, 10)
-plt.ylim(0, 2)
-x1 = np.linspace(0, 10, 8000)
-f = np.vectorize(f)
-y1 = f(x1)
-x2 = np.linspace(5, 7, 2000)  # Large weights
-y2 = f(x2)
-x3 = np.linspace(3, 5, 2000)
-y3 = f(x3)
-plt.plot(x1, y1, color='b', lw=2)
-plt.plot(x2, y2, color='g', lw=2)
-plt.plot(x3, y3, color='r', lw=2)
-plt.title("Approximating f(x)")
-plt.show()
+def draw():
+    global sigmoid, f
+    x1 = np.linspace(0, 10, 8000)
+    f = np.vectorize(f)
+    y1 = f(x1)
+    x = np.arange(-20, 20, 0.01)
+    w = 3
+    bais1 = 18.5
+    bais2 = 5.5
+    z = x * w
+    z1 = np.copy(z)
+    z2 = np.copy(z)
+    z1 -= bais1
+    z2 -= bais2
+    sigmoid = np.vectorize(sigmoid)
+    y2 = sigmoid(-z1)
+    y3 = sigmoid(z2)
+    plt.plot(x, y3, color='g', lw=2, label="Sigmoid +ve")
+    plt.plot(x, y2, color='r', lw=2, label="Sigmoid -ve")
+    plt.plot(x1, y1, color='b', lw=2, label="f(x)")
+    # Get values
+    plt.fill_between(x, [0], y2, where=(y3 >= y2), facecolor='red')
+    plt.fill_between(x, [0], y3, where=(y2 >= y3), facecolor='green')
+    plt.fill_between
+    plt.xlim(0, 10)
+    plt.ylim(0, 2)
+    plt.title("Approximation f(x)")
+    plt.legend()
+    plt.grid()
+    plt.show()
